6a0c1a82e8dc9703765c9752ce71fdec605c8944
Alvaro Polo <apoloval@gmail.com>
2015-09-14 17:21:03 +0200

    Implement types in `local` module

--- src/local.rs
+++ src/local.rs
@@ -7,27 +7,59 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use std::ffi::CString;
 use std::io;
+use std::ptr::null_mut;
 
-use super::{Handler, Session};
+use user32::FindWindowExA;
+use winapi::windef::HWND;
 
-pub struct LocalHandler;
+use super::{Handle, Session};
+use super::ipc::*;
 
-impl Handler for LocalHandler {
-    type Sess = LocalSession;
-    fn connect() -> io::Result<Self> { unimplemented!() }
-    fn session(&self) -> LocalSession { unimplemented!() }
-    fn disconnect(self) { unimplemented!() }
+pub struct LocalHandle {
+    handle: HWND,
 }
 
-pub struct LocalSession;
+impl Handle for LocalHandle {
+    type Sess = LocalSession;
+
+    fn connect() -> io::Result<Self> {
+        unsafe {
+            let win_name = CString::new("UIPCMAIN").unwrap();
+            let handle = FindWindowExA(
+                null_mut(), null_mut(), win_name.as_ptr(), null_mut());
+            if handle != null_mut() {
+                Ok(LocalHandle { handle: handle })
+            } else {
+                Err(io::Error::new(
+                    io::ErrorKind::ConnectionRefused,
+                    "cannot connect to local FSUIPC: cannot create window handle"))
+            }
+        }
+    }
+
+    fn session(&self) -> LocalSession {
+        LocalSession {
+            handle: self.handle,
+            buffer: io::Cursor::new(Vec::with_capacity(4096))
+        }
+    }
+
+    fn disconnect(self) {}
+}
+
+pub struct LocalSession {
+    handle: HWND,
+    buffer: io::Cursor<Vec<u8>>,
+}
 
 impl Session for LocalSession {
     fn read_bytes(&mut self, offset: u16, dest: *mut u8, len: usize) -> io::Result<usize> {
-        unimplemented!()
+        self.buffer.write_rsd(offset, dest, len)
     }
 
     fn write_bytes(&mut self, offset: u16, src: *const u8, len: usize) -> io::Result<usize> {
-        unimplemented!()
+        self.buffer.write_wsd(offset, src, len)
     }
 }
