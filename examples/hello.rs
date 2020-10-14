51d3adcdd63f419cfd45449a68457b737540f304
Alvaro Polo <apoloval@gmail.com>
2020-10-14 09:36:09 +0200

    Fix compilation warnings from modern Rust

--- examples/hello.rs
+++ examples/hello.rs
@@ -25,19 +25,19 @@ fn main() {
 }
 
 fn run() -> io::Result<()> {
-    let mut handle = try!(UserHandle::new());
+    let mut handle = UserHandle::new()?;
     let mut session = handle.session();
     let mut fsuipc_ver = 0u32;
     let mut fs_ver = 0u16;
     let mut hour = 0u8;
     let mut minute = 0u8;
     let mut second = 0u8;
-    try!(session.read(0x3304, &mut fsuipc_ver));
-    try!(session.read(0x3308, &mut fs_ver));
-    try!(session.read(0x0238, &mut hour));
-    try!(session.read(0x0239, &mut minute));
-    try!(session.read(0x023a, &mut second));
-    try!(session.process());
+    session.read(0x3304, &mut fsuipc_ver)?;
+    session.read(0x3308, &mut fs_ver)?;
+    session.read(0x0238, &mut hour)?;
+    session.read(0x0239, &mut minute)?;
+    session.read(0x023a, &mut second)?;
+    session.process()?;
     println!("FSUIPC version {:x}.{:x}", fsuipc_ver >> 28, fsuipc_ver >> 20);
     println!("FS/P3D version {}", fs_ver);
     println!("Simulation local time is {:02}:{:02}:{:02}", hour, minute, second);
